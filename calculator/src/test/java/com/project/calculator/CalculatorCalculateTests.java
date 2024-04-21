0d44b0d876506c40ba12518c3d109c1366d4e54a
egisatriadyw <egi.satria22@gmail.com>
2024-04-21 20:57:33 +0700

    commit calculate test

--- calculator/src/test/java/com/project/calculator/CalculatorCalculateTests.java
+++ calculator/src/test/java/com/project/calculator/CalculatorCalculateTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculatorApplicationTests {
+public class CalculatorCalculateTests {
 
 	@Test
 	public void testAddition() {
